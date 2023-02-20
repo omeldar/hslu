4f8fa1deabcf27efeceb375b44050329f054ca6d
Eldar Omerovic <eldar.work.edu@gmail.com>
2023-02-20 17:25:54 +0100

    Add E1 first two exercises

---
+++ AD/source/oop_maven_template/src/test/java/ch/hslu/sw01/AhaBeispielTest.java
@@ -0,0 +1,74 @@
+package ch.hslu.sw01;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AhaBeispielTest {
+    @Test
+    void SW01_E1_2_CountTaskRunsWith4(){
+        AhaBeispiel ahaBeispiel = new AhaBeispiel();
+        int n = 4;
+        ahaBeispiel.task(n);
+        assertEquals(4, ahaBeispiel.Task1RunCount);
+        assertEquals(12, ahaBeispiel.Task2RunCount);
+        assertEquals(32, ahaBeispiel.Task3RunCount);
+    }
+    @Test
+    void SW01_E1_2_CountTaskRunsWith8(){
+        AhaBeispiel ahaBeispiel = new AhaBeispiel();
+        int n = 8;
+        ahaBeispiel.task(n);
+        assertEquals(4, ahaBeispiel.Task1RunCount);
+        assertEquals(24, ahaBeispiel.Task2RunCount);
+        assertEquals(128, ahaBeispiel.Task3RunCount);
+    }
+    @Test
+    void SW01_E1_2_CountTaskRunsWith32(){
+        AhaBeispiel ahaBeispiel = new AhaBeispiel();
+        int n = 32;
+        ahaBeispiel.task(n);
+        assertEquals(4, ahaBeispiel.Task1RunCount);
+        assertEquals(96, ahaBeispiel.Task2RunCount);
+        assertEquals(2048, ahaBeispiel.Task3RunCount);
+    }
+
+    @Test
+    void SW01_E1_2_CountTaskRunsWith33(){
+        AhaBeispiel ahaBeispiel = new AhaBeispiel();
+        int n = 33;
+        ahaBeispiel.task(n);
+        assertEquals(4, ahaBeispiel.Task1RunCount);
+        assertEquals(99, ahaBeispiel.Task2RunCount);
+        assertEquals(2178, ahaBeispiel.Task3RunCount);
+    }
+
+    @Test
+    void SW01_E1_2_CountTaskRunsWith40(){
+        AhaBeispiel ahaBeispiel = new AhaBeispiel();
+        int n = 40;
+        ahaBeispiel.task(n);
+        assertEquals(4, ahaBeispiel.Task1RunCount);
+        assertEquals(120, ahaBeispiel.Task2RunCount);
+        assertEquals(3200, ahaBeispiel.Task3RunCount);
+    }
+
+    @Test
+    void SW01_E1_2_CountTaskRunsWith100(){
+        AhaBeispiel ahaBeispiel = new AhaBeispiel();
+        int n = 100;
+        ahaBeispiel.task(n);
+        assertEquals(4, ahaBeispiel.Task1RunCount);
+        assertEquals(300, ahaBeispiel.Task2RunCount);
+        assertEquals(20000, ahaBeispiel.Task3RunCount);
+    }
+
+    @Test
+    void SW01_E1_2_CountTaskRunsWith500(){
+        AhaBeispiel ahaBeispiel = new AhaBeispiel();
+        int n = 500;
+        ahaBeispiel.task(n);
+        assertEquals(4, ahaBeispiel.Task1RunCount);
+        assertEquals(1500, ahaBeispiel.Task2RunCount);
+        assertEquals(500000, ahaBeispiel.Task3RunCount);
+    }
+}
---
+++ AD/source/oop_maven_template/src/test/java/ch/hslu/sw01/GGTRechnerTest.java
@@ -0,0 +1,30 @@
+package ch.hslu.sw01;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GGTRechnerTest {
+    @Test
+    void SW01_E1_1_TestIterativ1(){
+        int a = 16;
+        int b = 12;
+        int ggt = GGTRechner.ggtIterativ1(a, b);
+        assertEquals(4, ggt);
+    }
+
+    @Test
+    void SW01_E1_1_TestIterativ2(){
+        int a = 16;
+        int b = 12;
+        int ggt = GGTRechner.ggtIterativ2(a, b);
+        assertEquals(4, ggt);
+    }
+
+    @Test
+    void SW01_E1_1_TestRekursiv(){
+        int a = 16;
+        int b = 12;
+        int ggt = GGTRechner.ggtRekursiv(a, b);
+        assertEquals(4, ggt);
+    }
+}
