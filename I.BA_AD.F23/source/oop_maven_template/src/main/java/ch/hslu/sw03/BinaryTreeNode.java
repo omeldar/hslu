9bebb3399b9fba3607ce0cc44bdf18bfa27f057d
Eldar Omerovic <eldar.work.edu@gmail.com>
2023-03-06 17:24:14 +0100

    Add custom tree data structure for binary search

---
+++ I.BA_AD.F23/source/oop_maven_template/src/main/java/ch/hslu/sw03/BinaryTreeNode.java
@@ -0,0 +1,49 @@
+package ch.hslu.sw03;
+
+import org.jetbrains.annotations.NotNull;
+
+public class BinaryTreeNode<TKey extends Comparable<TKey>, TValue>
+        implements Comparable<IBinaryTreeNode<TKey, TValue>>{
+
+    private TKey key;
+    private TValue value;
+    private BinaryTreeNode leftBinaryTreeNode;
+    private BinaryTreeNode rightBinaryTreeNode;
+
+    @Override
+    public IBinaryTreeNode getLeftNode() {
+        return leftBinaryTreeNode;
+    }
+
+    @Override
+    public IBinaryTreeNode getRightNode() {
+        return rightBinaryTreeNode;
+    }
+
+    @Override
+    public TKey getKey() {
+        return key;
+    }
+
+    @Override
+    public TValue getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (o == this)
+            return true;
+        if (!(o instanceof BinaryTreeNode))
+            return false;
+        if (this.value == ((BinaryTreeNode<?, ?>) o).getValue()){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int compareTo(@NotNull IBinaryTreeNode<TKey, TValue> o) {
+        return 0;   // needs to be implemented by following implementation rules of compareTo
+    }
+}
