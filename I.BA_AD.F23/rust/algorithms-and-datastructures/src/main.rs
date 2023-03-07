99ff89271c08b85c551f350b9c9aba29d2a2d0da
Eldar Omerovic <eldar.work.edu@gmail.com>
2023-03-07 03:00:51 +0100

    Implement custom binary tree in rust

---
+++ I.BA_AD.F23/rust/algorithms-and-datastructures/src/main.rs
@@ -0,0 +1,10 @@
+use binary_tree::binary_tree;
+
+fn main() {
+    let mut root = binary_tree::Node { val: &5, left: None, right: None };
+    root.insert(&3);
+    root.insert(&7);
+    root.insert(&2);
+    root.insert(&8);
+    println!("{:?}", root);
+}
