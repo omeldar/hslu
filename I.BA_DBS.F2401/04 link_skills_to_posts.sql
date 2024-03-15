b8a93bce190d1d3a0bec9944b4539207116f9f37
omeldar <eldar.work.edu@gmail.com>
2024-03-15 01:19:44 +0100

    Increase performance of script 4

--- I.BA_DBS.F2401/04 link_skills_to_posts.sql
+++ I.BA_DBS.F2401/04 link_skills_to_posts.sql
@@ -1,25 +1,29 @@
--- Step 1: Create a new table for unique skills
-CREATE TABLE unique_job_skills (
-    skill_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    skill_name TEXT UNIQUE
-);
-
--- Step 2: Create an associative table to map skills to linkedin_posts
-CREATE TABLE linkedin_post_skills (
-    post_id UUID, -- Assuming the linkedin_posts table has a UUID field named post_id
-    skill_id UUID,
-    PRIMARY KEY (post_id, skill_id),
-    FOREIGN KEY (post_id) REFERENCES linkedin_posts (id) ON DELETE CASCADE,
-    FOREIGN KEY (skill_id) REFERENCES unique_job_skills (skill_id) ON DELETE CASCADE
-);
-
--- Step 3: Populate the unique_skills table with unique skills
--- Insert unique skills and create associations in a loop
 DO $$
 DECLARE
     rec RECORD;
     _skill_id UUID;
 BEGIN
+
+    CREATE INDEX ON import_job_skills (job_link);
+    CREATE INDEX ON linkedin_posts (job_link);
+
+    -- Step 1: Create a new table for unique skills
+    CREATE TABLE unique_job_skills (
+        skill_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+        skill_name TEXT UNIQUE
+    );
+
+    -- Step 2: Create an associative table to map skills to linkedin_posts
+    CREATE TABLE linkedin_post_skills (
+        post_id UUID, -- Assuming the linkedin_posts table has a UUID field named post_id
+        skill_id UUID,
+        PRIMARY KEY (post_id, skill_id),
+        FOREIGN KEY (post_id) REFERENCES linkedin_posts (id) ON DELETE CASCADE,
+        FOREIGN KEY (skill_id) REFERENCES unique_job_skills (skill_id) ON DELETE CASCADE
+    );
+
+    -- Step 3: Populate the unique_skills table with unique skills
+    -- Insert unique skills and create associations in a loop
     FOR rec IN SELECT DISTINCT unnest(string_to_array(job_skills, ',')) AS skill, job_link
         FROM import_job_skills -- Replace with the actual table name
     LOOP
